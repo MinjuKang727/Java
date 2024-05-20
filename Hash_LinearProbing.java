bce24a854dd79d3b2a45d63ef14402774668bd14
MinjuKang <108849480+MinjuKang727@users.noreply.github.com>
2024-05-21 02:21:54 +0900

    Update Hash_LinearProbing.java

--- Hash_LinearProbing.java
+++ Hash_LinearProbing.java
@@ -12,13 +12,13 @@ public class Hash_LinearProbing {
 
         Slot(String key, String value) {
             this.key = key;
-            this.value = vaule;
+            this.value = value;
         }
     }
 
     // 해시 함수 구현
     public Integer hashFunction(String key) {
-        return (int)(key.charAt(0) % this.hashTable.length;
+        return (int)(key.charAt(0) % this.hashTable.length);
     }
 
     // 저장을 성공했는가
@@ -33,11 +33,14 @@ public class Hash_LinearProbing {
             } else {
                 Integer currAddress = address + 1;
 
-                while(this.hashTable[currAddress] != null {
+
+                while(this.hashTable[currAddress] != null) {
                     if (this.hashTable[currAddress].key == key) {
                         this.hashTable[currAddress].value = value;
                         return true;
                     } else {
+                        // 인덱스를 현재 인덱스에서 뒤쪽 인덱스로만 검색하므로
+                        // 실제로 비어있는 Slot이 존재하지만 false를 반환할 수도 있음
                         currAddress++;
                         if (currAddress >= this.hashTable.length) {
                             return false;
@@ -81,19 +84,20 @@ public class Hash_LinearProbing {
 
     public static void main(String[] args) {
 
-        Hash_LinearProbing myHsh = new Hash_LinearProbing(20);
+        Hash_LinearProbing myHash = new Hash_LinearProbing(20);
 
-        myHash.saveData("DaveLee","01094064180");
+        myHash.saveData("MinjuKang","01075043700");
         myHash.saveData("Lees","01098251090");
         myHash.saveData("James","01044444180");
         myHash.saveData("Daves","01012347180");
         myHash.saveData("Dalyes","01099999999");
+                                                          // Output
+        System.out.println(myHash.getData("Lees"));       // 01095251090
+        System.out.println(myHash.getData("James"));      // 01044444180
+        System.out.println(myHash.getData("MinjuKang"));  // 01075043700
+        System.out.println(myHash.getData("Daves"));      // 01012347180
+        System.out.println(myHash.getData("Dalyes"));     // 01099999999
+        System.out.println(myHash.getData("Doolly"));     // null
 
-        System.out.println(myHash.getData("Lees")); // 01095251090
-        System.out.println(myHash.getData("James")); // 01044444180
-        System.out.println(myHash.getData("DaveLee")); // 01094064180
-        System.out.println(myHash.getData("Daves")); // 01012347180
-        System.out.println(myHash.getData("Dalyes")); // 01099999999
-        System.out.println(myHash.getData("zzzz")); // null
     }
 }
